421166fc9eb5aa928b3e3010b605638e22e22720
Sergius <ololoshka@gmail.com>
2022-02-27 23:25:51 +0400

    deleted something and created a new project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let incr = 10,
-    decr = 10;
 
-// ++incr;
-// --decr;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(incr++);
-console.log(decr--);
+
+const firstFilm = prompt('Один из последних просмотренных фильмов?', ''),
+      firstPoints = prompt('На сколько оцените его?', ''),
+      secondFilm = prompt('Один из последних просмотренных фильмов?', ''),
+      secondPoints = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[firstFilm] = firstPoints;
+personalMovieDB.movies[secondFilm] = secondPoints;
+
+console.log(personalMovieDB);
